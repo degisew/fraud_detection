73a97df9acd1bf9ef42cb4af388725c588b5990f
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 13:33:49 +0300

    Add type hints and __str__ method

--- core/models.py
+++ core/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.conf import settings
 
 class Transaction(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    amout = models.PositiveIntegerField()
-    timestamp = models.DateTimeField(auto_now=True)
-    is_fraudlent = models.BooleanField()
+    user: str = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amout: int = models.PositiveIntegerField()
+    timestamp: str = models.DateTimeField(auto_now=True)
+    is_fraudlent: bool = models.BooleanField()
 
+
+    def __str__(self) -> int:
+        return self.amout
+    
 class FraudAlert(models.Model):
     pass
