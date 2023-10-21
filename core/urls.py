fbbd1c4933b835f2660ec1abf84b7d971b62aab2
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 21:22:32 +0300

    Added Routes

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import create_transaction
+from .views import create_transaction, user_login, user_logout, register
 urlpatterns = [
-    path('', create_transaction, name='create-transact')
+    path('', create_transaction, name='create-transact'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
