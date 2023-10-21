25a99eca0c37c181aa78bea325975cfde91f9a38
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 18:23:03 +0300

    Override save() method

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterable
 from django.db import models
 from django.conf import settings
 
@@ -5,11 +6,15 @@ class Transaction(models.Model):
     user: str = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     amount: int = models.PositiveIntegerField()
     timestamp: str = models.DateTimeField(auto_now=True)
-    is_fraudlent: bool = models.BooleanField()
+    is_fraudlent: bool = models.BooleanField(default=False)
+    
 
-
-    def __str__(self) -> int:
-        return self.amout
+    def save(self):
+        if self.amount > 10:
+            self.is_fraudlent = True
+        else:
+            self.is_fraudlent = False
+        return super().save()
     
 class FraudAlert(models.Model):
     pass
