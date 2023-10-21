aa22ffed115f7bc17fff07ec69f6b7601e9f5d90
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 21:22:05 +0300

    Created a userregistration and login form

--- core/form.py
+++ core/form.py
@@ -1,7 +1,39 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django.conf import settings
 from .models import Transaction
 
 class TransactionForm(ModelForm):
     class Meta:
         model = Transaction
         fields = ['user', 'amount']
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['first_name','last_name', 'username', 'email', 'password1' ,'password2']
+
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': 'Username',
+                                                             'class': 'form-control',
+                                                             }))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+                                                                 'class': 'form-control',
+                                                                 'data-toggle': 'password',
+                                                                 'id': 'password',
+                                                                 'name': 'password',
+                                                                 }))
+    remember_me = forms.BooleanField(required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
