c9965b833f7c25a0aa9aef66fc03c4c10493602b
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 18:24:38 +0300

    Fix admin list page

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,11 @@
+from typing import Any
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.db.models.query import QuerySet
 from .models import Transaction
 
-
-admin.site.register(Transaction)
-
-
+    
+@admin.register(Transaction)
+class TransactionAdmin(ModelAdmin):
+    list_display = ['user','amount', 'timestamp', 'is_fraudlent']
+    list_filter = ['is_fraudlent']
