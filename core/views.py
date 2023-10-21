db3154962538dc0d75657fe71079b207cca6f910
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 18:30:04 +0300

    Remove unwanted Implementation

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Transaction
 from .form import TransactionForm
 
 
@@ -10,10 +9,3 @@ def create_transaction(request):
     if form.is_valid():
         form.save()
     return render(request, 'core/transact_form.html', {'forms': form})
-
-def detect_fraud(request):
-    transactions = Transaction.objects.all()
-    for transaction in transactions:
-        if transaction.amount >10:
-            transaction.is_fraudlent = True
-    return HttpResponse('Detection Complete!')
