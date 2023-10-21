b8aeff44e3b2e2a1b5f7f893de1ad8658d6c4f71
Degisew Mengist <jegisew21@gmail.com>
2023-10-21 21:24:27 +0300

    fix route

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import create_transaction, user_login, user_logout, register
 urlpatterns = [
-    path('', create_transaction, name='create-transact'),
+    path('create/', create_transaction, name='create-transact'),
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
