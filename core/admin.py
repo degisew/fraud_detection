50a6d213d89ba0bd3fedbe5f0361b8d7a2459ff2
Degisew Mengist <jegisew21@gmail.com>
2023-10-23 23:08:19 +0300

    Registered moels

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,28 @@
-from typing import Any
-from django.contrib import admin
-from django.contrib.admin import ModelAdmin
-from django.db.models.query import QuerySet
-from .models import Transaction
 
-    
+from django.contrib import admin
+# from django.contrib.admin import ModelAdmin
+# from django.db.models.query import QuerySet
+# from .models import Transaction
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import CustomUser, Transaction
+
+def mark_as_fraud(modeladmin, request, queryset):
+    for user in queryset:
+        user.is_fraud = True
+        user.save()
+
+mark_as_fraud.short_description = "Mark selected users as potential fraud"
+
+class CustomUserAdmin(UserAdmin):
+    actions = [mark_as_fraud]
+    list_display = ['first_name', 'last_name', 'email', 'is_fraud']
+    list_filter = ['is_fraud']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.unregister(User)
+
 @admin.register(Transaction)
-class TransactionAdmin(ModelAdmin):
-    list_display = ['user','amount', 'timestamp', 'is_fraudlent']
-    list_filter = ['is_fraudlent']
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'timestamp']
