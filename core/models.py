886a806a7b55e3dcc77aa90c1a08816f2d2b22d4
Degisew Mengist <jegisew21@gmail.com>
2023-10-23 23:07:54 +0300

    Create custom ser model

--- core/models.py
+++ core/models.py
@@ -1,21 +1,25 @@
+
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group, Permission
 from django.db import models
-# from django.conf import settings
-
-
+from django.conf import settings
 
 
 class CustomUser(AbstractUser):
     is_fraud = models.BooleanField(default=False)
+    groups = models.ManyToManyField(Group, related_name='customuser_set')
 
+    # Define a unique related_name for the user_permissions field
+    user_permissions = models.ManyToManyField(
+        Permission, related_name='customuser_set'
+    )
     def __str__(self):
         return self.username
     
 class Transaction(models.Model):
-    user: str = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='transaction')
+    user: str = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transaction')
     amount: int = models.PositiveIntegerField()
     timestamp: str = models.DateTimeField(auto_now=True)
-    # is_fraudlent: bool = models.BooleanField(default=False)
     
 
     # def save(self):
